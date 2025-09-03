0086ae6522314d9ff938657abf604946b2e38dc0
palomapaldino5 <pombadapaz@gmail.com>
2025-09-03 11:44:15 -0300

    atualizado api

--- src/main/java/br/com/edukacode/api/DadosListagemLead.java
+++ src/main/java/br/com/edukacode/api/DadosListagemLead.java
@@ -1,5 +1,8 @@
 package br.com.edukacode.api;
 
-public class DadosListagemLead {
-
+public record DadosListagemLead(String nome,String email,String telefone) 
+{
+    public DadosListagemLead(Lead lead) {
+        this(lead.getNome(), lead.getEmail(), lead.getTelefone());
+    }
 }
